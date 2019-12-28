c2e140d0b572ed636f9bbb27d2e2edf0e555f79f
PeterM <petermedvesek@gmail.com>
2019-12-28 16:42:27 +0100

    -> with cubeMX set pull ups on external interruts pins

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -95,8 +95,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PBPin PBPin PBPin PBPin */
   GPIO_InitStruct.Pin = idx_int0_Pin|idx_int1_Pin|idx_int2_Pin|idx_int3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
